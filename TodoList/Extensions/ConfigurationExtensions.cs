2217857f640f18793c19d1a28e20126369660dd2
Vladyslav Geyna <ipz215_gvs@student.ztu.edu.ua>
2023-04-04 22:38:23 +0300

    Added config extension class

---
+++ TodoList/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace TodoList.Extensions
+{
+	public static class ConfigurationExtensions
+	{
+		public static string? GetDefaultConnectionString(this IConfiguration configuration)
+		{
+			try
+			{
+				return configuration.GetConnectionString("DefaultConnection");
+			}
+			catch
+			{
+				throw new Exception("Wrong connection string name");
+			}
+		}
+	}
+}
