9271e341d8b38c2066606c08a4f27c0b14674c34
Vladyslav Geyna <ipz215_gvs@student.ztu.edu.ua>
2023-04-04 23:05:35 +0300

    Created model classes

---
+++ TodoList/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Models
+{
+	public class Category
+	{
+		[Key]
+		public int Id { get; set; }
+		public string Name { get; set; } = null!;
+		public IEnumerable<Category>? Categories { get; set; }
+	}
+}
---
+++ TodoList/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Models
+{
+	public class Task
+	{
+		[Key]
+		public int Id { get; set; }
+		public string Description { get; set; } = null!;
+		public DateTime DueDate { get; set; }
+		public DateTime DateOfCreation { get; set; }
+		public bool IsCompleted { get; set; }
+		public int? CategoryId { get; set; }
+		public Category? Category { get; set; }
+	}
+}
