8a2d1246ea366e5ba56f4752267f53e972eae4c2
Vladyslav Geyna <ipz215_gvs@student.ztu.edu.ua>
2023-04-10 12:31:09 +0300

    Refactoring

--- TodoList/TodoList/Data/DapperContext.cs
+++ TodoList/TodoList/Data/DapperContext.cs
@@ -6,15 +6,18 @@ namespace TodoList.Data
 {
 	public sealed class DapperContext
 	{
-		private readonly string _connectionString;
-        public DapperContext(IConfiguration configuration)
+		private readonly string _connectionString; 
+		
+		public DapperContext(IConfiguration configuration)
         {
-			_connectionString = configuration.GetDefaultConnectionString() 
-			                    ?? throw new Exception("Cannot get connection string");
-		}
+	        _connectionString = configuration.GetDefaultConnectionString()
+	                            ?? throw new Exception("Cannot get connection string");
+        }
+		
 		public IDbConnection CreateConnection()
 		{
 			return new SqlConnection(_connectionString);
 		}
+		
     }
 }
