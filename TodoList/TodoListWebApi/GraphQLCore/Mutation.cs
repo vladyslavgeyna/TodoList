39c037f08fe415ed1aed1a9c34012de5b9236158
Vladyslav Geyna <ipz215_gvs@student.ztu.edu.ua>
2023-05-11 22:44:55 +0300

    Implemented other mutations for deleting and updating

--- TodoList/TodoListWebApi/GraphQLCore/Mutation.cs
+++ TodoList/TodoListWebApi/GraphQLCore/Mutation.cs
@@ -25,6 +25,7 @@ namespace TodoListWebApi.GraphQLCore
         private readonly DapperContext _context;
         private readonly XmlStorageService _xmlStorageService;
         private readonly IHttpContextAccessor _httpContextAccessor;
+
         public Mutation(DapperContext dapperContext,
             XmlStorageService xmlStorageService,
             IHttpContextAccessor httpContextAccessor)
@@ -42,6 +43,15 @@ namespace TodoListWebApi.GraphQLCore
                     return task;
                 });
 
+            Field<StringGraphType>("deleteTask")
+                .Argument<NonNullGraphType<IntGraphType>>("id")
+                .ResolveAsync(async context =>
+                {
+                    int id = context.GetArgument<int>("id");
+                    await _taskRepository.DeleteByIdAsync(id);
+                    return "Task was deleted successfully";
+                });
+
             Field<TaskType>("createCategory")
                 .Argument<NonNullGraphType<CategoryInputType>>("category")
                 .ResolveAsync(async context =>
@@ -49,6 +59,15 @@ namespace TodoListWebApi.GraphQLCore
                     var category = context.GetArgument<Category>("category");
                     await _categoryRepository.AddAsync(category);
                     return category;
+                });
+
+            Field<StringGraphType>("deleteCategory")
+                .Argument<NonNullGraphType<IntGraphType>>("id")
+                .ResolveAsync(async context =>
+                {
+                    int id = context.GetArgument<int>("id");
+                    await _categoryRepository.DeleteByIdAsync(id);
+                    return "Category was deleted successfully";
                 });
 
             Field<StringGraphType>("changeStorageType")
@@ -61,6 +80,30 @@ namespace TodoListWebApi.GraphQLCore
                     httpContextAccessor?.HttpContext?.Response.Cookies.Append(StorageCookieHelper.CookieName, storage.ToString());
                     return storage.ToString();
                 });
+
+            FieldAsync<StringGraphType>("changeTaskStatus", arguments: new QueryArguments(
+                new QueryArgument<NonNullGraphType<IntGraphType>>()
+                {
+                    Name = "id"
+                },
+                new QueryArgument<NonNullGraphType<BooleanGraphType>>()
+                {
+                    Name = "isCompleted"
+                }),
+                resolve: async context =>
+                {
+                    int id = context.GetArgument<int>("id");
+                    bool status = context.GetArgument<bool>("isCompleted");
+                    var task = await _taskRepository.GetByIdAsync(id);
+                    if (task is null)
+                    {
+                        return "Task was not found";
+                    }
+                    task.IsCompleted = status;
+                    await _taskRepository.UpdateByIdAsync(id, task);
+                    return "Task status was changed successfully";
+                }
+            );
         }
     }
 }
